8160186d3f29d25cae066069d5c91bf2f21aca65
lakshmi ps <lakshmisasikumar27@gmail.com>
2023-05-17 11:47:26 +0530

    second commit

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pickle
-st.title('medical diagnostic prediction app')
+st.title('Medical Diagnostic Prediction App😊')
+st.markdown('Does the Person have Diabetics')
 
-#step1:load the trained model
+#step1 : load the trained model
 model=open('rfc.pickle','rb')
 clf=pickle.load(model)
 model.close()
 
+#step2: get the user input from frontend
 
-#step 2:get the user input from front end
 pregs=st.number_input('Pregnancies',0,20,step=1)
-glucose=st.slider('Glucose',40,200,40)
+glucose=st.slider('Glucose',42,200,40)
 bp=st.slider('BloodPressure',20,140,20)
 skin=st.slider('SkinThickness',7,99,7)
 insulin=st.slider('Insulin',14,850,14)
@@ -22,21 +23,24 @@ dpf=st.slider('DiabetesPedigreeFunction',0.05,2.50,0.05)
 age=st.slider('Age',21,90,21)
 
 
-#step3 convert user input to model input
+#step 3: COnvert user input to model input
+
 data={
     'Pregnancies':pregs,
-    'Glucose': glucose,
+    'Glucose':glucose,
     'BloodPressure':bp,
     'SkinThickness':skin,
     'Insulin':insulin,
     'BMI':bmi,
     'DiabetesPedigreeFunction':dpf,
-    'Age':age}
+    'Age':age
+}
+
 input_data=pd.DataFrame([data])
 
-#step4: get the predictions and print the result
-prediction=clf.predict(input_data)[0]
-if st.button('predict'):
+# step4 : get the predictions and print the result
+prediction = clf.predict(input_data)[0]
+if st.button("Predict"):
     if prediction==0:
         st.write("The Person is Healthy")
     if prediction==1:
